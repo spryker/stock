e01826edb16b705b8c84bfb82a9a10202e3d1489
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-10-17 18:05:43 +0200

    feature/pim: added PluginAbstractManager, separation between before and after, updated tests, cleanups

--- src/Spryker/Zed/Stock/Communication/Plugin/ProductConcreteAfterUpdatePlugin.php
+++ src/Spryker/Zed/Stock/Communication/Plugin/ProductConcreteAfterUpdatePlugin.php
@@ -15,7 +15,7 @@ use Spryker\Zed\Product\Dependency\Plugin\ProductConcretePluginInterface;
  * @method \Spryker\Zed\Stock\Business\StockFacadeInterface getFacade()
  * @method \Spryker\Zed\Stock\Communication\StockCommunicationFactory getFactory()
  */
-class ProductConcreteCreatePlugin extends AbstractPlugin implements ProductConcretePluginInterface
+class ProductConcreteAfterUpdatePlugin extends AbstractPlugin implements ProductConcretePluginInterface
 {
 
     /**
